cfb35aab50df1a2d0cda4b157b684f31d735aee8
solymosiviktor1 <72075604+solymosiviktor1@users.noreply.github.com>
2020-11-03 12:22:21 +0100

    Add files via upload

---
+++ f0003b.py
@@ -0,0 +1,6 @@
+vezetéknév = input('Mi a vezetékneved? ')
+keresztnév = input('Mi a keresztneved? ')
+év = input ('melyik évben születtél? ')
+csillagjegy =input ('Melyik csillagjegyben születtél? ')
+print('A te neved')
+
